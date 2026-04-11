7a4fd0ea2d1eb3d8cad6ee754fbd33f6d7861f3f
Ans Khan <anskhan997@gmail.com>
2026-04-10 19:10:52 -0500

    Test full CI/CD

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from DockerHub + Kubernetes + Github Actions"}
+    return {"message": "CI/CD working on AKS 🚀"}
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
